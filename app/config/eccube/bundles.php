659ad7cff64783147901176756efece9581bf48c
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2025-09-13 16:34:38 +0900

    composer: skipthedragon/inertia-bundleを追加

--- app/config/eccube/bundles.php
+++ app/config/eccube/bundles.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * This file is part of EC-CUBE
- *
- * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
- *
- * http://www.ec-cube.co.jp/
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
@@ -28,4 +17,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Eccube\EccubeBundle::class => ['all' => true],
+    Rompetomp\InertiaBundle\InertiaBundle::class => ['all' => true],
 ];
